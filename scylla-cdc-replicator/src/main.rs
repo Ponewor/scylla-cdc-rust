729f6cb7dfff36bd43025acde1add685a8bcb509
Marcin Mazurek <marcinek.mazurek@gmail.com>
2022-05-10 15:00:48 +0200

    Make replicator executable

--- scylla-cdc-replicator/src/main.rs
+++ scylla-cdc-replicator/src/main.rs
@@ -1,29 +1,84 @@
 mod replication_tests;
+mod replicator;
 pub mod replicator_consumer;
 
-use replicator_consumer::ReplicatorConsumerFactory;
-use scylla::SessionBuilder;
-use std::error::Error;
-use std::sync::Arc;
 use std::time::Duration;
 
+use clap::Parser;
+use futures_util::stream::FusedStream;
+use futures_util::StreamExt;
+use tokio::select;
+use tokio::time::sleep;
+
+use crate::replicator::Replicator;
+
+#[derive(Parser)]
+struct Args {
+    /// Keyspace name
+    #[clap(short, long)]
+    keyspace: String,
+
+    /// Table names provided as a comma delimited string
+    #[clap(short, long)]
+    table: String,
+
+    /// Address of a node in source cluster
+    #[clap(short, long)]
+    source: String,
+
+    /// Address of a node in destination cluster
+    #[clap(short, long)]
+    destination: String,
+
+    /// Window size in seconds
+    #[clap(long, default_value_t = 60.)]
+    window_size: f64,
+
+    /// Safety interval in seconds
+    #[clap(long, default_value_t = 30.)]
+    safety_interval: f64,
+
+    /// Sleep interval in seconds
+    #[clap(long, default_value_t = 10.)]
+    sleep_interval: f64,
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello");
-    tokio::time::sleep(Duration::from_secs(1)).await;
+async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let tables: Vec<_> = args.table.split(',').map(|s| s.to_string()).collect();
+    let sleep_interval = Duration::from_secs_f64(args.sleep_interval);
 
-    // Create a factory to remove the warning about unused new() function.
-    let _factory = ReplicatorConsumerFactory::new(
-        Arc::new(
-            SessionBuilder::new()
-                .known_node("127.0.0.1:9042")
-                .build()
-                .await?,
-        ),
-        "ks".to_string(),
-        "t".to_string(),
-    );
+    let (mut replicator, mut handles) = Replicator::new(
+        args.source,
+        args.keyspace.clone(),
+        tables.clone(),
+        args.destination,
+        args.keyspace,
+        tables,
+        Duration::from_secs_f64(args.window_size),
+        Duration::from_secs_f64(args.safety_interval),
+        sleep_interval,
+    )
+    .await?;
 
-    println!("Replicator");
-    Ok(())
+    loop {
+        select! {
+            _ = tokio::signal::ctrl_c() => {
+                println!("Shutting down...");
+                replicator.stop();
+                while handles.next().await.is_some() {}
+                return Ok(())
+            }
+            res = handles.next(), if !handles.is_terminated() => {
+                let res = res.unwrap();
+                if res.is_err() {
+                    replicator.stop();
+                    while handles.next().await.is_some() {}
+                    return res;
+                }
+            }
+        }
+        sleep(sleep_interval).await;
+    }
 }
